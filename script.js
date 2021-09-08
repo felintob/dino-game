91ba953e0cdcc7d1b835e4112a0010acaeee76e8
felintob <bfelinto@gmail.com>
2021-09-08 10:47:33 -0300

    Criando cactus

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background')
+let isJumping = false;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
+        if (!isJumping) {
         jump();
+        }
     }
 }
 
 function jump() {
     let position = 0;
+
+    isJumping = true;
+
     let upInterval = setInterval(() => {
         if (position >= 150) {
             clearInterval(upInterval);
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
+                    isJumping = false;
                 } else {
                     position -= 20;
                     dino.style.bottom = position + 'px';
@@ -27,4 +35,14 @@ function jump() {
 
 }
 
+function creatCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+}
+
+creatCactus();
 document.addEventListener('keyup', handleKeyUp);
